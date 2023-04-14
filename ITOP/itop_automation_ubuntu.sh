96737857998bcc8ce258e0d129625edd3aebe3f6
An Duy Le <101538840+anldori@users.noreply.github.com>
2023-04-14 14:00:07 +0700

    Update itop_automation_ubuntu.sh

--- ITOP/itop_automation_ubuntu.sh
+++ ITOP/itop_automation_ubuntu.sh
@@ -1,22 +1,22 @@
 echo 'UPDATE & UPGRADE PACKAGES'
 echo '--------------------'
-sudo apt update && sudo apt upgrade -y
+sudo apt -qq update && sudo apt -qq upgrade -y
 
 echo 'INSTALLING LAMPSTACK'
 echo '--------------------'
 echo 'Installing Apache 2...'
-sudo apt install apache2 -y && systemctl enable apache2 && systemctl start apache2
+sudo apt -qq install apache2 -y && systemctl enable apache2 && systemctl start apache2
 echo 'Done.'
 echo
 echo 'Installing PHP 8.2 and its extensions...'
 sudo add-apt-repository ppa:ondrej/php -y
-sudo apt-get install php php-mysql php-ldap php-mcrypt php-cli php-soap php-json graphviz -y
-sudo apt-get install php-xml php-gd php-zip php-mbstring php-curl libapache2-mod-php -y
-sudo apt install php-bz2 php-sqlite3 -y
+sudo apt -qq install php php-mysql php-ldap php-mcrypt php-cli php-soap php-json graphviz -y
+sudo apt -qq install php-xml php-gd php-zip php-mbstring php-curl libapache2-mod-php -y
+sudo apt -qq install php-bz2 php-sqlite3 -y
 echo 'Done.'
 echo
 echo 'Installing MySQL...'
-sudo apt-get install mysql-server mysql-client -y
+sudo apt -qq install mysql-server mysql-client -y
 echo 'Done.'
 systemctl restart apache2
 
@@ -32,7 +32,7 @@ echo 'Setup Database completely.'
 echo
 
 echo 'Setup Web Directory'
-sudo apt install unzip wget -y
+sudo apt -qq install unzip wget -y
 cd /var/www/html/
 rm * -rf
 wget https://onboardcloud.dl.sourceforge.net/project/itop/itop/3.0.3/iTop-3.0.3-10998.zip
